3d155fd45266ee222a1102618f4b525733a20515
zsarge <mcseamusc@gmail.com>
2020-06-25 17:49:32 -0400

    Add test button
    Sketch now reads input from a button, and displays the button state.
    Proper formatting has also been applied.

--- MintDuinoV2.ino
+++ MintDuinoV2.ino
@@ -1,28 +1,38 @@
-/* 
-* MintDuino V2
-* Goal: Play games on an arduino, in an Altoids tin.
-* Status: Unfinished
-* 
-* This code is released under the MIT license.
-* If a copy of the license is not included, 
-* it can be found at https://opensource.org/licenses/MIT
-*/
+/*
+ * MintDuino V2
+ * Goal: Play games on an arduino, in an Altoids tin.
+ * Status: Unfinished
+ *
+ * This code is released under the MIT license.
+ * If a copy of the license is not included,
+ * it can be found at https://opensource.org/licenses/MIT
+ */
 
 // LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
-#include <LiquidCrystal_I2C.h> // Library for LCD
-#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h>  // Library for LCD
+#include <Wire.h>               // Library for I2C communication
 
-LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // takes (address, width, height)
+LiquidCrystal_I2C lcd =
+    LiquidCrystal_I2C(0x27, 16, 2);  // takes (address, width, height)
+
+const int buttonPin = 7;
+int buttonState = 0;
 
 void setup() {
-  lcd.init();
-  lcd.clear();
-  lcd.backlight();
+    pinMode(buttonPin, INPUT);
+
+    lcd.init();
+    lcd.clear();
+    lcd.backlight();
 }
 
 void loop() {
-  lcd.setCursor(0,0);
-  lcd.print("Hello World!"); 
-  lcd.setCursor(2,1);
-  lcd.print(millis());
+    buttonState = digitalRead(buttonPin);
+
+    lcd.setCursor(0, 0);
+    if (buttonState == HIGH) {
+        lcd.print(buttonState);
+    } else {
+        lcd.print(buttonState);
+    }
 }
