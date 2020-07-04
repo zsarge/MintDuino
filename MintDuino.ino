f298423db117c82e0cb4bd067a4b974462b2fbbf
zsarge <mcseamusc@gmail.com>
2020-07-04 15:43:05 -0400

    Refactor buttons with structs
    Buttons are now handled inside of structs,
    which makes them easier to handle in the code.

--- MintDuino.ino
+++ MintDuino.ino
@@ -1,5 +1,5 @@
 /*
- * MintDuino V2
+ * MintDuino
  * Goal: Play games on an arduino, in an Altoids tin.
  * Status: Hardware finished, software unfinished
  *
@@ -19,7 +19,7 @@
  * I2C Wiring Guide:
  * I2C LCD Controller   Arduino
  * ............ GND ---- GND
- * ............ VCC ---- 5 V
+ * ............ VCC ---- 5V
  * ............ SDA ---- A4
  * ............ SCL ---- A5
  */
@@ -29,46 +29,59 @@ LiquidCrystal_I2C lcd =
 
 /*
  *  Button layout on device:
- *  __1____2__
+ *  __A____B__
  *  [ Screen ]
- *  [_3____4_]
+ *  [_C____D_]
  *
- * The user's index fingers rest on 1 and 2
- * The user's thumbs rest on 3 and 4
+ * The user's index fingers rest on A and B
+ * The user's thumbs rest on C and D
  */
 
-const int buttonPin = 8;
-const int buttonPin2 = 7;
-const int buttonPin3 = 6;
-const int buttonPin4 = 5;
-int buttonState = 0;
-int buttonState2 = 0;
-int buttonState3 = 0;
-int buttonState4 = 0;
+typedef struct {
+    byte pin;
+    bool state;
+    int timeHeldDown;
+} Button;
+
+typedef struct {
+    Button A;
+    Button B;
+    Button C;
+    Button D;
+    bool allHeldDown;
+    bool oneHeldDown;
+} Buttons;
+
+Button A = {8, 0, 0};
+Button B = {7, 0, 0};
+Button C = {6, 0, 0};
+Button D = {5, 0, 0};
+Buttons btns = {A, B, C, D, 0, 0};
 
 void setup() {
-    pinMode(buttonPin, INPUT);
-    pinMode(buttonPin2, INPUT);
-    pinMode(buttonPin3, INPUT);
-    pinMode(buttonPin4, INPUT);
+    pinMode(btns.A.pin, INPUT);
+    pinMode(btns.B.pin, INPUT);
+    pinMode(btns.C.pin, INPUT);
+    pinMode(btns.D.pin, INPUT);
 
     lcd.init();
     lcd.clear();
     lcd.backlight();  // Turns on backlight. Default is off.
+    lcd.display();
 }
 
 void loop() {
-    buttonState = digitalRead(buttonPin);
-    buttonState2 = digitalRead(buttonPin2);
-    buttonState3 = digitalRead(buttonPin3);
-    buttonState4 = digitalRead(buttonPin4);
+    btns.A.state = digitalRead(btns.A.pin);
+    btns.B.state = digitalRead(btns.B.pin);
+    btns.C.state = digitalRead(btns.C.pin);
+    btns.D.state = digitalRead(btns.D.pin);
 
-    lcd.setCursor(0, 0);
-    lcd.print(buttonState);
+    lcd.setCursor(0, 1);
+    lcd.print(btns.A.state);
     lcd.print(", ");
-    lcd.print(buttonState2);
+    lcd.print(btns.B.state);
     lcd.print(", ");
-    lcd.print(buttonState3);
+    lcd.print(btns.C.state);
     lcd.print(", ");
-    lcd.print(buttonState4);
+    lcd.print(btns.D.state);
 }
